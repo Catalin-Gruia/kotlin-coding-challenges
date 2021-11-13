82a502c57faa561c84ae82d7ffcd42151e9b7cd7
Catalin Gruia <cataliingruia@gmail.com>
2021-11-13 13:13:58 +0200

    Working implementation of Factorial

--- src/test/kotlin/com/igorwojda/integer/factorial/challenge.kt
+++ src/test/kotlin/com/igorwojda/integer/factorial/challenge.kt
@@ -4,7 +4,10 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun factorial(n: Int): Int {
-    TODO("not implemented")
+    var total = 1
+    for (number in 1 .. n)
+        total *= number
+    return total
 }
 
 class RecursiveFactorial {
