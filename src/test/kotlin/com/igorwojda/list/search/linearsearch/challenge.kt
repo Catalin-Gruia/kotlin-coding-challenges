78eba97cc69bf2620b19806538adbc3923918eac
Catalin Gruia <cataliingruia@gmail.com>
2021-11-13 13:01:33 +0200

    Working implementation of Linear search

--- src/test/kotlin/com/igorwojda/list/search/linearsearch/challenge.kt
+++ src/test/kotlin/com/igorwojda/list/search/linearsearch/challenge.kt
@@ -4,7 +4,10 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun getIndex(list: List<String>, str: String): Int {
-    TODO("not implemented")
+    for (item in list.indices)
+        if (list[item] == str)
+            return item
+    return -1
 }
 
 private class Test {
