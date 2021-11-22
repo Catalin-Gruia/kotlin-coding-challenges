fc4ddfc8ff0436ef41bcb031e7c7e91779955f96
Catalin Gruia <cataliingruia@gmail.com>
2021-11-22 14:16:35 +0200

    Changed implementation from List to Map
    Changed implementation from List to Map so that it contains unique items

--- src/test/kotlin/com/igorwojda/list/countuniquevalues/challenge.kt
+++ src/test/kotlin/com/igorwojda/list/countuniquevalues/challenge.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun countUniqueValues(list: List<Int>): Int {
-    return list.distinct().size
+    return list.toSet().size
 }
 
 private class Test {
