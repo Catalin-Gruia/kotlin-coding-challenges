471683463a56929096d7d6efc6c0f2403e580cf7
Catalin Gruia <cataliingruia@gmail.com>
2021-11-23 14:51:32 +0200

    Implemented Reverse string challenge
    Implemented Reverse string challenge

--- src/test/kotlin/com/igorwojda/string/reverse/challenge.kt
+++ src/test/kotlin/com/igorwojda/string/reverse/challenge.kt
@@ -4,7 +4,10 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 
 private fun reverse(str: String): String {
-    TODO("not implemented")
+    if (str.isEmpty()) {
+        return str
+    }
+    return reverse(str.substring(1)) + str[0]
 }
 
 private class Test {
